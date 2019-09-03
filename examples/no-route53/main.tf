96a6c9c750e5469e5707b62ff2031671825ed3b8
Niek Palm <npalm@users.noreply.github.com>
2019-09-03 21:34:35 +0200

    Add GitHub actions (#8)
    * Migration to Terraform012 (#7)
    
    * Upgrade module to terraform 0.12
    
    * Upgrade module to terraform 0.12
    
    * Update to tf 0.12
    
    * Add github actions
    
    * Add github actions
    
    * Add github actions

--- examples/no-route53/main.tf
+++ examples/no-route53/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "eu-west-1"
-  version = "2.1.0"
+  version = "2.22.0"
 }
 
 module "cloudfront" {
@@ -31,7 +31,8 @@ output "output" {
   description = "Module outputs."
 
   value = {
-    cloudfront_dns_record = "${module.cloudfront.cloudfront_dns_record}"
-    cloudfront_bucket_arn = "${module.cloudfront.cloudfront_bucket_arn}"
+    cloudfront_dns_record = module.cloudfront.cloudfront_dns_record
+    cloudfront_bucket_arn = module.cloudfront.cloudfront_bucket_arn
   }
 }
+
